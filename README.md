67f00f30cc7bee313a00343b5c547e1cfff97e24
MACS-KINCAID <mitsiu.carreno@upa.edu.mx>
2026-02-08 03:44:13 -0600

    update tests and readme

--- README.md
+++ README.md
@@ -33,3 +33,65 @@ elm-test
 - elm-test 
 - elm-format
 - elm-review
+
+# Exercises
+## Records exercises
+1.0 Let's define a record for programming languages with:
+- name : String
+- releaseYear : Int
+- currentVersion: String
+
+1.1 Create a list with at least two programming languages       
+1.2 Create a function "languageNames" that receives the list from point 1.1 and generates a String list with only the names eg:      
+- Input : [{name="elm", releaseYear= 2012, currentVersion="0.19.1"},{name="javascript", releaseYear= 1995, currentVersion="ECMAScript 2025"}]  
+- Output: ["elm", "javascript"]
+
+
+## Records exercises
+2.0. Let's define a record for user with:
+- name : String
+- uType : String     
+
+2.1 Create a list of users (uType can be "Student" or "Professor")      
+2.2 Create a function "onlyStudents" that receives the list from point 2.1 and generates a String list with only the name of the "Student" (professors names are returned as an empty string) eg:
+- Input : [{name="Roberto", uType= "Student"}, {name="Mitsiu", uType="Professor"}]
+- Output: ["Roberto", ""]
+
+## Aliases exercise
+3.0 Let's define a record for games aliased "Videogame":
+- title : String
+- releaseYear : Int
+- available: Bool
+- downloads: Int
+- genres : List String
+
+3.1 Create a list with at least two videogames       
+3.2 Create a function "getVideogameGenres" that receives the list from point 1.2 and generates a List of List of strings with only the genres eg:      
+- Input : [{title="Control", releaseYear=2019, ... genres=["Action", "Shooter"]}, {title="Ocarina of time", ... genres=["Action", "Adventure"]}]
+- Output: [["Action", "Shooter"], ["Action", "Adventure"]]
+
+## Html exercise
+Let's define a record named "Computer" with:
+- ram: String
+- model: String
+- brand: String
+- screenSize: String
+
+And create a variable "myLaptop" of type Computer
+       
+
+Finally, let's make a variable "main" that reduces to:
+```html
+<div>
+  <h1>My laptop<h1>
+  <div>
+    <ul>
+      <li>Ram: {{.ram myLaptop}}</li>
+      <li>Modelo: {{.model myLaptop}}</li>
+      <li>Marca: {{.brand myLaptop}}</li>
+      <li>Pulgadas: {{.screenSize myLaptop}}</li>
+    </ul>
+  </div>
+</div>
+```
+
---
+++ tests/TestSuite.elm
@@ -0,0 +1,205 @@
+module TestSuite exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Helper exposing (..)
+import Test exposing (..)
+import Test.Html.Query as Query
+import Test.Html.Selector exposing (containing, exactText, tag, text)
+
+
+languageFuzzer :
+    Fuzzer
+        { name : String
+        , releaseYear : Int
+        , currentVersion : String
+        }
+languageFuzzer =
+    Fuzz.map3
+        (\name releaseYear currentVersion ->
+            { name = name
+            , releaseYear = releaseYear
+            , currentVersion = currentVersion
+            }
+        )
+        Fuzz.string
+        Fuzz.int
+        Fuzz.string
+
+
+languageNamesTest : Test
+languageNamesTest =
+    describe "Testing languageNames function"
+        [ test "languageNames []" <|
+            \_ ->
+                languageNames []
+                    |> Expect.equal []
+        , test "languageNames [{name=\"elm\", releaseYear= 2012, currentVersion=\"0.19.1\"},{name=\"javascript\", releaseYear= 1995, currentVersion=\"ECMAScript 2025\"}]" <|
+            \_ ->
+                languageNames [ { name = "elm", releaseYear = 2012, currentVersion = "0.19.1" }, { name = "javascript", releaseYear = 1995, currentVersion = "ECMAScript 2025" } ]
+                    |> Expect.equal [ "elm", "javascript" ]
+        , fuzz (list languageFuzzer) "languageNames should return a list of strings" <|
+            \fuzzList ->
+                languageNames fuzzList
+                    |> Expect.equal (List.map .name fuzzList)
+        ]
+
+
+uTypeFuzzer : Fuzzer String
+uTypeFuzzer =
+    Fuzz.oneOf
+        [ Fuzz.constant "Student"
+        , Fuzz.constant "Professor"
+        ]
+
+
+userFuzzer :
+    Fuzzer
+        { name : String
+        , uType : String
+        }
+userFuzzer =
+    Fuzz.map2
+        (\name uType ->
+            { name = name
+            , uType = uType
+            }
+        )
+        Fuzz.string
+        uTypeFuzzer
+
+
+onlyStudentsTest : Test
+onlyStudentsTest =
+    describe "Testing onlyStudents function"
+        [ test "onlyStudents []" <|
+            \_ ->
+                onlyStudents []
+                    |> Expect.equal []
+        , test "onlyStudents [{name=\"Roberto\", uType= \"Student\"}, {name=\"Mitsiu\", uType=\"Professor\"}]" <|
+            \_ ->
+                onlyStudents [ { name = "Roberto", uType = "Student" }, { name = "Mitsiu", uType = "Professor" } ]
+                    |> Expect.equal [ "Roberto", "" ]
+        , fuzz (list userFuzzer) "onlyStudents should return only the name of Student type" <|
+            \fuzzList ->
+                onlyStudents fuzzList
+                    |> Expect.equal
+                        (List.map
+                            (\u ->
+                                case .uType u of
+                                    "Student" ->
+                                        .name u
+
+                                    _ ->
+                                        ""
+                            )
+                            fuzzList
+                        )
+        ]
+
+
+videogameAliasedFuzzer : Fuzzer Videogame
+videogameAliasedFuzzer =
+    Fuzz.map5 Videogame
+        Fuzz.string
+        Fuzz.int
+        Fuzz.bool
+        Fuzz.int
+        (Fuzz.list Fuzz.string)
+
+
+getVideogameGenresTest : Test
+getVideogameGenresTest =
+    describe "Testing getVideogameGenres function"
+        [ test "getVideogameGenres []" <|
+            \_ ->
+                getVideogameGenres []
+                    |> Expect.equal []
+        , test "getVideogameGenres [{title=\"Control\", releaseYear=2019, available=True, downloads=1234567, genres=[\"Action\", \"Shooter\"]}, {title=\"Ocarina of time\", releaseyear=1998, available=False, downloads=12345, genres=[\"Action\", \"Adventure\"]}]" <|
+            \_ ->
+                getVideogameGenres [ { title = "Control", releaseYear = 2019, available = True, downloads = 1234567, genres = [ "Action", "Shooter" ] }, { title = "Ocarina of time", releaseYear = 1998, available = False, downloads = 12345, genres = [ "Action", "Adventure" ] } ]
+                    |> Expect.equal [ [ "Action", "Shooter" ], [ "Action", "Adventure" ] ]
+        , fuzz (list videogameAliasedFuzzer) "getVideogameGenres should return a List List String" <|
+            \fuzzList ->
+                getVideogameGenres fuzzList
+                    |> Expect.equal (List.map (\vg -> .genres vg) fuzzList)
+        ]
+
+
+htmlTest : Test
+htmlTest =
+    describe "Testing html generated"
+        [ test "main should have a <div><h1>My computer</h1></div>" <|
+            \() ->
+                main
+                    |> Query.fromHtml
+                    |> Query.has
+                        [ tag "div"
+                        , containing
+                            [ tag "h1" ]
+                        , containing
+                            [ exactText "My laptop" ]
+                        ]
+        , test ("main should have a \n<div>\n  <div>\n    <ul>\n      <li></li>\n    </ul>\n  </div>\n</div>\nwith content \"Ram: " ++ .ram myLaptop ++ "\"") <|
+            \() ->
+                main
+                    |> Query.fromHtml
+                    |> Query.has
+                        [ tag "div"
+                        , containing
+                            [ tag "div" ]
+                        , containing
+                            [ tag "ul" ]
+                        , containing
+                            [ tag "li" ]
+                        , containing
+                            [ exactText ("Ram: " ++ .ram myLaptop) ]
+                        ]
+        , test ("main should have a \n<div>\n  <div>\n    <ul>\n      <li></li>\n    </ul>\n  </div>\n</div>\nwith content \"Modelo: " ++ .model myLaptop ++ "\"") <|
+            \_ ->
+                main
+                    |> Query.fromHtml
+                    |> Query.has
+                        [ tag "div"
+                        , containing
+                            [ tag "div" ]
+                        , containing
+                            [ tag "ul" ]
+                        , containing
+                            [ tag "li" ]
+                        , containing
+                            [ exactText ("Modelo: " ++ .model myLaptop) ]
+                        ]
+        , test ("main should have a \n<div>\n  <div>\n    <ul>\n      <li></li>\n    </ul>\n  </div>\n</div>\nwith content \"Marca: " ++ .brand myLaptop ++ "\"") <|
+            \_ ->
+                main
+                    |> Query.fromHtml
+                    |> Query.has
+                        [ tag "div"
+                        , containing
+                            [ tag "div" ]
+                        , containing
+                            [ tag "ul" ]
+                        , containing
+                            [ tag "li" ]
+                        , containing
+                            [ exactText ("Marca: " ++ .brand myLaptop) ]
+                        ]
+        , test
+            ("main should have a \n<div>\n  <div>\n    <ul>\n      <li></li>\n    </ul>\n  </div>\n</div>\nwith content \"Pulgadas: " ++ .screenSize myLaptop ++ "\"")
+          <|
+            \_ ->
+                main
+                    |> Query.fromHtml
+                    |> Query.has
+                        [ tag "div"
+                        , containing
+                            [ tag "div" ]
+                        , containing
+                            [ tag "ul" ]
+                        , containing
+                            [ tag "li" ]
+                        , containing
+                            [ exactText ("Pulgadas: " ++ .screenSize myLaptop) ]
+                        ]
+        ]
